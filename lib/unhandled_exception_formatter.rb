a3d53fc95157a0391978031470264db07764e3ff
Ryo Nakamura <r7kamura@gmail.com>
2022-06-17 08:42:24 +0900

    Remove unused code

--- lib/unhandled_exception_formatter.rb
+++ lib/unhandled_exception_formatter.rb
@@ -1,8 +1,3 @@
 # frozen_string_literal: true
 
 require_relative 'unhandled_exception_formatter/version'
-
-module UnhandledExceptionFormatter
-  class Error < StandardError; end
-  # Your code goes here...
-end
