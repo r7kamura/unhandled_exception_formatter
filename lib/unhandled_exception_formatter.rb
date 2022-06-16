5efabdcdf3907df51b724bad54f2f05ee39407aa
Ryo Nakamura <r7kamura@gmail.com>
2022-06-17 08:06:47 +0900

    bundle gem unhandled_exception_formatter

---
+++ lib/unhandled_exception_formatter.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "unhandled_exception_formatter/version"
+
+module UnhandledExceptionFormatter
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/unhandled_exception_formatter/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module UnhandledExceptionFormatter
+  VERSION = "0.1.0"
+end
